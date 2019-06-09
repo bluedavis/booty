1992dbc01c8db9cc37e02caa03a57c09567d22cc
Blue Davis <34557724+bluedavis@users.noreply.github.com>
2019-06-09 13:08:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     print request.form
-    return "Hey this is blue, who apparently is in denial and doesn't know how to express her feelings and makes eveyrhting sooooo difficult with her mind. But really its toally fine because I'm not crazy because if you don't think you'll loose your brain"
+    return "Hi"
